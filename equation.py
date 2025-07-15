529caf87dbdb3f66ae5b7f1f2ff89844739384f9
alwinalbert <160037555+alwinalbert@users.noreply.github.com>
2025-07-15 23:37:30 +0530

    equation

--- equation.py
+++ equation.py
@@ -1,15 +1,22 @@
-import numpy as np  
+import numpy as np
 import matplotlib.pyplot as plt
-
 n = np.arange(-10,11,1)
-
 def u(n):
     return np.where(n>=0,1,0)
 
-result = u(n) - u(n-1)
+def x(n):
+    return (-1.0)**n * 2.0**n * (u(n)-u(n-5))
 
-plt.title("impulse fn from u(n) - u(n-1)")
+result = x(n-2) + x(n+3)
+
+plt.title("equation question")
 plt.xlabel("time")
 plt.ylabel("amplitude")
 plt.stem(n,result)
 plt.show()
+
+
+
+
+
+
