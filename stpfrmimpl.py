a9e2d3880ee6596175bcb48f934803faf9e78550
alwinalbert <160037555+alwinalbert@users.noreply.github.com>
2025-07-15 23:17:57 +0530

    impulse fn from stepfns

---
+++ stpfrmimpl.py
@@ -0,0 +1,16 @@
+import numpy as np  
+import matplotlib.pyplot as plt
+
+n = np.arange(-10,11,1)
+
+def u(n):
+    
+    return np.where(n>=0,1,0)
+
+result = u(n) - u(n-1)
+
+plt.title("impulse fn from u(n) - u(n-1)")
+plt.xlabel("time")
+plt.ylabel("amplitude")
+plt.stem(n,result)
+plt.show()
